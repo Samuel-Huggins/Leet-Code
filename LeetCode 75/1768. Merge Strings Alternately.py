1ed427133c1ed7fc85c332141ae3d7ca86fdb3e3
Samuel Huggins <sjhuggins123@gmail.com>
2026-04-24 16:03:11 +0100

    Should've used the min function to choose the length, much more elegant. then just append the left over characters depending on which word is longer

--- LeetCode 75/1768. Merge Strings Alternately.py
+++ LeetCode 75/1768. Merge Strings Alternately.py
@@ -1,17 +1,13 @@
 class Solution(object):
     def mergeAlternately(self, word1, word2):
-        """
-        :type word1: str
-        :type word2: str
-        :rtype: str
-        """
-        looplength = 0
-        if len(word1) <= len(word2):
-            looplength = len(word2)
-        else:
-            looplength = len(word1)
-        word3 = ("" * looplength)
-        for i in range(0, looplength):
-            word3 = word3 + (word1[i])
-            word3 = word3 + (word2[i])
+        word3 = ""
+        min_len = min(len(word1), len(word2))
+
+        for i in range(min_len):
+            word3 += word1[i]
+            word3 += word2[i]
+
+        word3 += word1[min_len:]
+        word3 += word2[min_len:]
+
         return word3
